b907e7c1c5bab1facd99fb84c00b0954eaf2be5a
Wali Subi Bipin <wali.subibipin2024@vitstudent.ac.in>
2026-09-11 10:10:15 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-"""
-LivingMemoryOS - Streamlit Application Entrypoint
-Run with: streamlit run app.py
-"""
-
-from src.ui.dashboard import render_dashboard
-
-if __name__ == "__main__":
-    render_dashboard()
